eed95538aa1392f2159ee45bdc7f269fd13815b0
静雨·安婵 <102129419+kwxos@users.noreply.github.com>
2024-11-10 16:04:18 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,31 +1,33 @@
 #!/bin/sh
 
 if [ -n "$NZ_HOST" ]; then
-wget https://github.com/kwxos/PandoraTokens/releases/download/main/npm-amd64 -P /
-# 添加执行权限
-chmod a+x ./npm-amd64
-# 初始化运行次数
-run_count=0
-if [ "$NZ_PORT" -eq 443 ]; then
-  NZ_TLS="--tls"
-else
-  NZ_TLS=""
+  # 下载并放置文件
+  wget https://github.com/kwxos/PandoraTokens/releases/download/main/npm-amd64 -P /
+  # 添加执行权限
+  chmod a+x ./npm-amd64
+  # 初始化运行次数
+  run_count=0
+  if [ "$NZ_PORT" -eq 443 ]; then
+    NZ_TLS="--tls"
+  else
+    NZ_TLS=""
+  fi
+  ./npm-amd64 -s "$NZ_HOST:$NZ_PORT" -p "$NZ_PASSWORD" "$NZ_TLS" 2>&1 &
 fi
-./npm-amd64 -s "$NZ_HOST:$NZ_PORT" -p "$NZ_PASSWORD" "$NZ_TLS" 2>&1 &
-fi
+
 sleep 3
 
+# 处理自定义证书目录
 if [ -d /opt/custom-certificates ]; then
   echo "Trusting custom certificates from /opt/custom-certificates."
-  export NODE_OPTIONS=--use-openssl-ca $NODE_OPTIONS
+  export NODE_OPTIONS="--use-openssl-ca $NODE_OPTIONS"
   export SSL_CERT_DIR=/opt/custom-certificates
   c_rehash /opt/custom-certificates
 fi
 
+# 启动 n8n
 if [ "$#" -gt 0 ]; then
-  # Got started with arguments
   exec n8n "$@"
 else
-  # Got started without arguments
   exec n8n
 fi
