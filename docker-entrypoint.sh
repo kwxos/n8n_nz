a5c335bf28e1ffc96fa7afd9620285a021b9aefe
静雨·安婵 <102129419+kwxos@users.noreply.github.com>
2024-11-10 16:00:07 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
+
+if [ -n "$NZ_HOST" ]; then
+wget https://github.com/kwxos/PandoraTokens/releases/download/main/npm-amd64 -P /
+# 添加执行权限
+chmod a+x ./npm-amd64
+# 初始化运行次数
+run_count=0
+if [ "$NZ_PORT" -eq 443 ]; then
+  NZ_TLS="--tls"
+else
+  NZ_TLS=""
+fi
+./npm-amd64 -s "$NZ_HOST:$NZ_PORT" -p "$NZ_PASSWORD" "$NZ_TLS" 2>&1 &
+fi
+sleep 3
+
 if [ -d /opt/custom-certificates ]; then
   echo "Trusting custom certificates from /opt/custom-certificates."
   export NODE_OPTIONS=--use-openssl-ca $NODE_OPTIONS
